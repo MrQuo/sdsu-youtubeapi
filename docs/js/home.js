ba4e4a1262d7772bc7af35aea58bd2a3c423d664
Unknown <robloxguy1@gmail.com>
2017-08-10 03:08:12 -0700

    Index.html + CSS + JS upgrades.
    Fleshed out the homepage's look, adding search and minor filters along with WIP javascripting.

--- docs/js/home.js
+++ docs/js/home.js
@@ -1,33 +1,56 @@
 //Google YouTube API Function
-function start() {
-  // 2. Initialize the JavaScript client library.
-  gapi.client.init({
-    'apiKey': 'AIzaSyC__E8TsENRXiRqAcxYB9-Uk8AwSSyVhqI',
-    // Your API key will be automatically added to the Discovery Document URLs.
-    'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
-    // clientId and scope are optional if auth is not required.
-    'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
-    'scope': 'profile',
-  }).then(function() {
-    // 3. Initialize and make the API request.
-    return gapi.client.people.people.get({
-      'resourceName': 'people/me',
-      'requestMask.includeField': 'person.names'
-    });
-  }).then(function(response) {
-    console.log(response.result);
-  }, function(reason) {
-    console.log('Error: ' + reason.result.error.message);
+$('#searchForm').submit(function(e) {
+  e.preventDefault();
+  gapi.client.setApiKey('AIzaSyC__E8TsENRXiRqAcxYB9-Uk8AwSSyVhqI');
+  gapi.client.load('youtube', 'v3', function() {
+    makeRequest();
   });
-  //Test
+
   document.getElementById('test').innerHTML = 'YouTubeAPI loaded!';
   document.getElementById('test').style.color = 'red';
   document.getElementById('test').style.position = 'relative';
-};
+});
+
+function makeRequest() {
+        var q = document.getElementById('searchBar').value;
+        var request = gapi.client.youtube.search.list({
+                q: q,
+                part: 'snippet', 
+                maxResults: 10
+        });
+        request.execute(function(response)  {                                                               
+                var results = document.getElementById('resultDiv');
+                var srchItems = response.result.items;                      
+                srchItems.forEach(function(index, item) {
+                vidTitle = item.snippet.title;  
+                vidThumburl =  item.snippet.thumbnails.default.url;                 
+                vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image Available." style="width:204px;height:128px"></pre>';                   
+
+                results.append('<pre>' + vidTitle + vidThumbimg +  '</pre>');                      
+        });  
+    });  
+}
 
 function wordToCloud() {
 
 }
 
+function ifDisabled() {
+  document.getElementById('rangeTextMin').disabled = !(document.getElementById('rangeTextMin').disabled)
+  // document.getElementById('rangeRange').disabled = !(document.getElementById('rangeRange').disabled)
+}
+
+function ifDisabledMax() {
+  document.getElementById('rangeTextMax').disabled = !(document.getElementById('rangeTextMax').disabled)
+  // document.getElementById('rangeRange').disabled = !(document.getElementById('rangeRange').disabled)
+}
+
+function updateTextV(value) {
+  document.getElementById('rangeTextMax').value = value;
+}
+
+function updateRangeV(value) {
+  document.getElementById('rangeRange').value = value;
+}
+
 // 1. Load the JavaScript client library.
-gapi.load('client', start);
