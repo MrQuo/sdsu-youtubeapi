3517b843cc2faca74b22f101e4296c82d0ff8d8f
MrQuo <21177895+MrQuo@users.noreply.github.com>
2020-01-12 22:33:41 -0800

    js cleanup, key fresh, use d3.cloud.layout.js

--- docs/js/home.js
+++ docs/js/home.js
@@ -1,205 +1,180 @@
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-// Global Variables / Constants
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
+// Global Variables
+var g_words = [];
+var g_useLocation = false;
 
-var cloudArr = [];
-var locationToggle = false;
-
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-// Initialize Google API function
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-
-//Initialize, set submit function, use API
+// Initializes Google API
 function start() {
   gapi.client.init({
-    'apiKey': 'AIzaSyC__E8TsENRXiRqAcxYB9-Uk8AwSSyVhqI'
+    'apiKey': 'AIzaSyDJaKmkZTLl0ZUOO1t9G5HrLiomWCPn0n4' // Restricted key for Github Pages.
   }).then(() => {
-    $('#searchForm').submit((e) => {
-      e.preventDefault(); // <=========  Stops page refresh.
-
-      // Load YouTubeAPI, THEN call for a request, show results.
+    // Query YouTube Data API when search is sent
+    document.getElementById('searchForm').addEventListener('submit', (e) => {
+      e.preventDefault();
       gapi.client.load('youtube', 'v3').then(() => {
-        makeRequest();
+        formatAPIRequest(10).execute((response) => {
+          showResults(response);
+        });
         document.getElementById('resultDiv').style.display = 'inline-block';
       });
     });
   });
 }
 
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-// Request function
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-
-function makeRequest() {
-  // Declare parameters for request.
-  var q = document.getElementById('keywordBar').value;
-  var orderType = document.getElementById('orderDropdown').value;
-  var maxResults = 10;
-
-  var request;
-
-
-  // Make sure I'm getting the right values for my parameters.
-  // console.log(orderType);
-
-  // Check to use location-parameters or normal parameters
-  if (locationToggle == true) {
-
-    var locationString = document.getElementById('latitudeText').value + ',' + document.getElementById('longitudeText').value;
-
-    // Checked to make sure location formatted correctly.
-    // console.log(locationString);
-
-    request = gapi.client.youtube.search.list({
-      q: q,
-      safeSearch: 'strict',
-      part: 'snippet',
-      relevanceLanguage: 'en-US',
-      maxResults: maxResults,
-      type: 'video',
-      order: orderType,
-      regionCode: 'US',
-      location: locationString,
-      locationRadius: document.getElementById('radiusText').value + document.getElementById('radiusUnit').value
-    });
-  } else {
-    request = gapi.client.youtube.search.list({
-      q: q,
-      safeSearch: 'strict',
-      part: 'snippet',
-      relevanceLanguage: 'en-US', 
-      maxResults: maxResults,
-      type: 'video',
-      order: orderType,
-      regionCode: 'US'
-    });
+// Creates a pending YouTube Data API request/query with the chosen filters.
+function formatAPIRequest(maxResults = 10) {
+  var filters = {
+    part: 'snippet',
+    q: document.getElementById('keywordBar').value,
+    safeSearch: 'strict',
+    relevanceLanguage: 'en-US',
+    maxResults: maxResults,
+    type: 'video',
+    order: document.getElementById('orderDropdown').value,
+    regionCode: 'US'
+  };
+  if (g_useLocation) {
+    filters.location = document.getElementById('latitudeText').value + ','
+                        + document.getElementById('longitudeText').value;
+    filters.locationRadius = document.getElementById('radiusText').value
+                              + document.getElementById('radiusUnit').value;
   }
 
-  // Execute and send the request, GET the response.
-  request.execute((response) => {      
-    
-    // Logs the response of the request for Development purposes.
-    // console.log(response);
+  return gapi.client.youtube.search.list(filters);
+};
 
-    var results = $('#resultDiv');
-    results.empty();
-    var searchItems = response.result.items; 
-
-    results.append('<h3>Results (Top ' + maxResults + '):</h3>')
-    
-    searchItems.forEach((item) => {
-      vidTitleSnippet = item.snippet.title
-      vidTitle = '<span class="videoTitle">' + item.snippet.title + '</span>';
-
-      //Add title words to word cloud
-      titleToArr(item, cloudArr);
-
-      vidDesc = '<span class="videoDesc">' + item.snippet.description + '</span>';
-      vidThumb = '<a class="infoLink" href="https://youtube.com/watch?v=' + item.id.videoId + '" target="_blank" >' + '<img class="videoThumb" src="' + item.snippet.thumbnails.medium.url + '" alt="No Image Available.">' + '</a>';                 
-      results.append('<hr>' + '<div>' + vidThumb + '<div class="videoInfo">' + '<a class="infoLink" href="https://youtube.com/watch?v=' + item.id.videoId + '" target="_blank" >' + vidTitle + '</a>' + '<br>' + vidDesc + "</div>" + '</div>');                      
-    });
-    if (searchItems.length == 0) {
-      results.append('<hr><p style="color:red;">Oh no! Looks like there\'s no results.</p>');
-    }
-    wordsToCloud(cloudArr);  
-  });
-}
-
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-// Word-Cloud Functions!!
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-
-// Break title into words and sort into an object array for D3 - Algorithm
-function titleToArr(video, wordArr) {
-  var vidTitleArr = video.snippet.title.replace(/[.,\/#!$%\^&\*\'\";:{}\[\]=\-_`~()]/g,"").toLowerCase().split(' ');
+// Displays YouTube Data API search results from the response.
+function showResults(response) {
+  var results = $('#resultDiv');
+  var responseItems = response.result.items;
   
-  console.log('This is the vid title: ', vidTitleArr);
+  results.empty();
+  if (responseItems.length == 0) {
+    results.append('<hr><p style="color:red;">Oh no! It looks like there\'s no results.</p>');
+  } else {
+    // Show results in the results element and add the title to the wordcloud list.
+    results.append('<h3>Results: (Showing first ' + responseItems.length + ')</h3>')
+    g_words = [];
+    responseItems.forEach((item) => {
+      vidLink = 'https://youtube.com/watch?v=' + item.id.videoId
+      vidTitle = '<span class="videoTitle">' + item.snippet.title + '</span>';
+      vidDesc = '<span class="videoDesc">' + item.snippet.description + '</span>';
+      vidThumb = '<a class="infoLink" href="' + vidLink + '" target="_blank" >'
+                  + '<img class="videoThumb" src="' + item.snippet.thumbnails.medium.url
+                  + '" alt="No Image Available."' + '></a>';
+      results.append('<hr>' + '<div>' + vidThumb + '<div class="videoInfo">'
+                      + '<a class="infoLink" href="' + vidLink + '" target="_blank" >'
+                      + vidTitle + '</a>' + '<br>' + vidDesc
+                      + "</div>" + '</div>');
+      addToWordCount(item.snippet.title);
+    });
+  }
+  updateWordCloud();
+}
 
-  // ALGORITHM
+// Appends any "word" in a string to the given array.
+function addToWordCount(string) {
+  // Split words, names, or terms in titles
+  var words = string.toLowerCase().replace(/[^\w#$\'\’\-]+/g,'.').split('.');
+  g_words = g_words.concat(words);
+}
 
-  // For each word in the title..
-  vidTitleArr.forEach((word) => {
+// Update the wordcloud contents and draw on webpage.
+function updateWordCloud() {
+  var wordCloud = [];
+  var wordFreq = getFrequencyDict(g_words);
 
-    // ..if the wordArr is empty, just add it..
-    if (wordArr.length == 0) {
-      if (word != '') {
-        wordArr.push({text: word, size: 100});
-      }
-    } 
-    // ..else: if the wordArr already has the word, find the object and increase its size.. ELSE, add it 
-    else {
-      var found = false;
-      var index;
-      for(var i = 0; i < wordArr.length; i++) {
-        if (wordArr[i].text == word) {
-          found = true;
-          index = i;
-          break;
-        }
-      }
-      if (found) {
-        wordArr[index].size++;
-      } else {
-        if (word != '') {
-          wordArr.push({text: word, size: 100});
-        }
-      }
+  // Build D3 word-cloud object
+  Object.keys(wordFreq).forEach((word) => {
+    wordCloud.push({
+      'text': word,
+      'size': wordFreq[word] * 180 // px sqrt scale
+    });
+  });
+
+  // Redraw cloud
+  $('#word-cloud').empty().append('<h3>Word Cloud of Titles: </h3>');
+  d3.layout.cloud().size([700, 300])
+      .words(wordCloud)
+      .fontSize(function(d) { return Math.sqrt(d.size); })
+      .rotate(0)
+      .on('end', drawCloud)
+      .start();
+}
+
+// Draw D3 wordcloud on webpage.
+function drawCloud(items) {
+  d3.select('#word-cloud')
+    .append('svg').attr('width', 650).attr('height', 240).attr('class', 'wordcloud')
+    .append('g').attr('transform', 'translate(250,120)').selectAll('text')
+    .data(items).enter()
+    .append('text')
+    .style('font-size', function(d) { return d.size + 'px'; })
+    .style('fill', '#000')
+    .attr("transform", function(d) {
+      return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+    })
+    .text(function(d) { return d.text; });
+}
+
+// Returns a frequency dictionary of array's items (key) and occurences (value).
+function getFrequencyDict(arr) {
+  var dictionary = {};
+  arr.forEach((item) => {
+    if (dictionary[item] === undefined) {
+      dictionary[item] = 1;
+    } else {
+      dictionary[item] = dictionary[item] + 1;
     }
   });
+
+  return dictionary;
 }
 
-// Create word-cloud into HTML, reset wordbank when done ----- stop when you add page-switches
-function wordsToCloud(wordArr) {
-  // Testing my wordbank
-  wordArr.forEach((item) => {
-    console.log(item);
-  });
-
-  $('#word-cloud').empty();
-  d3.wordcloud().size([600, 800]).selector('#word-cloud').words(wordArr).scale('log').font('Verdana').start();
-  wordArr.length = 0;
+// Toggles minimum range search filter
+function toggleMin() {
+  var minInput = document.getElementById('rangeTextMin');
+  minInput.disabled = !minInput.disabled;
 }
 
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-// HTML-CSS Javascript DOM Manipulation
-// _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-
-// Input enable/disable functions
-// -----------------------------------------------------------
-// Views
-function ifDisabledMin() {
-  var elem = document.getElementById('rangeTextMin');
-  elem.disabled = !elem.disabled;
+// Toggles maximum range search filter
+function toggleMax() {
+  var maxInput = document.getElementById('rangeTextMax');
+  maxInput.disabled = !maxInput.disabled;
 }
 
-function ifDisabledMax() {
-  var elem = document.getElementById('rangeTextMax');
-  elem.disabled = !elem.disabled;
-}
-
-// Location
-function ifDisabledLocation() {
-  longitudeBox = document.getElementById('longitudeText');
-  latitudeBox = document.getElementById('latitudeText');
-  radiusInput = document.getElementById('radiusText');
-  radiusUInput = document.getElementById('radiusUnit');
+// Toggles geolocation search filter
+function toggleLocation() {
+  var longitudeBox = document.getElementById('longitudeText');
+  var latitudeBox = document.getElementById('latitudeText');
+  var radiusInput = document.getElementById('radiusText');
+  var unitInput = document.getElementById('radiusUnit');
 
   longitudeBox.disabled = !longitudeBox.disabled;
   latitudeBox.disabled = !latitudeBox.disabled;
   radiusInput.disabled = !radiusInput.disabled;
-  radiusUInput.disabled = !radiusUInput.disabled;
+  unitInput.disabled = !unitInput.disabled;
 
   longitudeBox.required = !longitudeBox.required;
   latitudeBox.required = !latitudeBox.required;
   radiusInput.required = !radiusInput.required;
 
-  // Toggle on/off the location-requests.
-  locationToggle = !locationToggle;
+  g_useLocation = !g_useLocation;
 }
 
 // _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
-// Run some functions on start.
+// Post-Declarations
 // _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
 
-// Loads Google API, callback: start()
+document.getElementById('viewToggleMin').addEventListener('onchange', (e) => {
+  toggleMin();
+});
+
+document.getElementById('viewToggleMax').addEventListener('onchange', (e) => {
+  toggleMax();
+});
+
+document.getElementById('locationToggle').addEventListener('onchange', (e) => {
+  toggleLocation();
+});
+
 gapi.load('client', start);
